30ff5b33dc1358a9cbcfa4584161b9b640c6e245
Andrew M <me@r3nya.ru>
2013-02-11 23:45:26 +0400

    Enter valid code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :company, :first_name, :last_name
-  validates :first_name, :company, presence: trueU
+  validates :first_name, :company, presence: true
   has_one :contact_info_type
 end
